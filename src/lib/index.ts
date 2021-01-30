f2991d41c33af245dc520492e0661c6f5d79fdc7
qinmingjie <1004537433@qq.com>
2021-01-30 17:32:20 +0800

    refactor(优化目录结构)

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,6 +1,6 @@
-export { default as Switch } from './Switch.vue';
-export { default as Button } from './Button.vue';
-export { default as Tabs } from './Tabs.vue';
-export { default as Tab } from './Tab.vue';
-export { default as Dialog } from './Dialog.vue';
-export { openDialog as openDialog } from './openDialog';
+export { default as Switch } from './Switch/Switch.vue';
+export { default as Button } from './Button/Button.vue';
+export { default as Tabs } from './Tab/Tabs.vue';
+export { default as Tab } from './Tab/Tab.vue';
+export { default as Dialog } from './Dialog/Dialog.vue';
+export { openDialog as openDialog } from './Dialog/openDialog';
