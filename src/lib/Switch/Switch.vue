39aadaf4dfe76aa0c7dcf1a14284a89e0ffb3088
qinmingjie <1004537433@qq.com>
2021-02-08 18:55:12 +0800

    refactor(优化switch组件代码)

--- src/lib/Switch/Switch.vue
+++ src/lib/Switch/Switch.vue
@@ -4,8 +4,8 @@
   </button>
 </template>
 <script lang="ts" setup="props, context">
-import { ref, SetupContext } from "vue";
-declare const props: {value: boolean}
+import { SetupContext } from "vue";
+// declare const props: {value: boolean}
 declare const context: SetupContext
 export default {
   props: {
@@ -18,22 +18,5 @@ export const toggle = () => {
 </script>
 
 <style lang="scss">
-$h: 22px;
-$h2: $h - 4px;
-.ideal-switch {
-  height: $h; width: $h * 2; border: none; background: #bfbfbf; border-radius: $h/2; position: relative;
-  > span {
-    position: absolute; top: 2px; left: 2px; height: $h2; width: $h2; background: white; border-radius: $h2 / 2; transition: all 250ms;
-  }
-  &.ideal-checked { background: #1890ff;
-    > span { left: calc(100% - #{$h2} - 2px); }
-  }
-  &:focus { outline: none; }
-  &:active {
-    > span { width: $h2 + 4px; }
-  }
-  &.ideal-checked:active {
-    > span { width: $h2 + 4px; margin-left: -4px; }
-  }
-}
+@import './_style.scss';
 </style>
